58cbab0cf33a21ffb17cc0753c0cff73ad34e367
Annadurai <anna2durai@gmail.com>
2024-11-29 17:00:19 +0530

    Update twitter.py

--- py/twitter/twitter.py
+++ py/twitter/twitter.py
@@ -15,10 +15,10 @@
 import tweepy
 from tweepy import OAuthHandler
  
-consumer_key = '443eBSdMKWu4sBQ3NZ9Bo94ciC5'
-consumer_secret = '44A0zmqMiAFszx1qclJRAxh4g4HOa5P8p9pNsjjQmqI8zlAe9dmH'
-access_token = '44484527396-mz8aVftwUMTtv1XKm9T070h9PkwDQvUxeG6PZ7Kb'
-access_secret = '44pb1CQELQJ2G4y5SeTNvYID7h099QGj1IV1gucCtrZwgEP'
+consumer_key = 'AA443eBSdMKWu4sBQ3NZ9Bo94ciC5AA'
+consumer_secret = 'AA44A0zmqMiAFszx1qclJRAxh4g4HOa5P8p9pNsjjQmqI8zlAe9dmHAA'
+access_token = 'AA44484527396-mz8aVftwUMTtv1XKm9T070h9PkwDQvUxeG6PZ7KbAA'
+access_secret = 'AA44pb1CQELQJ2G4y5SeTNvYID7h099QGj1IV1gucCtrZwgEPAA'
  
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
